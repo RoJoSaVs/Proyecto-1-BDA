4a4202317d01dbc80348fbb62d429472dcdbf193
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2021-09-30 11:39:42 -0600

    added the main components (empty) and bootstrap to the angular project

--- frontend1-BDA/src/app/app.module.ts
+++ frontend1-BDA/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ApplicantComponent } from './applicant/applicant.component';
+import { HumanResourcesComponent } from './human-resources/human-resources.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApplicantComponent,
+    HumanResourcesComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
